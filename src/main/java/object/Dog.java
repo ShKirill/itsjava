0c14c511901b3181033abeca838b7e1f952d7c6b
ShKirill <mr.muld@gmail.com>
2022-03-18 12:22:10 +0300

    1th

---
+++ src/main/java/object/Dog.java
@@ -0,0 +1,58 @@
+package object;
+
+import object.Dog;
+
+public class Dog {
+	private final String nickname;
+	private int age;
+ 
+	public Dog(String nickname, int age) {
+		this.nickname = nickname;
+		this.age = age;
+	}
+ 
+	public int getAge() {
+		return age;
+	}
+ 
+	public void setAge(int age) {
+		this.age = age;
+	}
+ 
+	@Override
+	public int hashCode() {
+		final int prime = 31; // константа prime
+		int result = 1;
+		result = prime * result + age; // получение значения result для age
+		result = prime * result + ((nickname == null) ? 0 : nickname.hashCode()); // получение значения result для
+																					// никнейма
+		return result; // возврат result
+	}
+ 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) { // проверка совпадения текущего экземпляра с переданным объектом obj
+			return true; // возврат значение true
+		}
+		if (!(obj instanceof Dog)) {// проверка что obj не являеться собакой
+			return false; // возврат значение false
+		}
+		Dog other = (Dog) obj; // явное приведение класса
+		if (age != other.age) { // проверка не равенства возраста
+			return false; // возврат значение false
+		}
+		if (nickname == null) { // проверка что никнейм равен нулю
+			if (other.nickname != null) { // проверка что другой никнейм не равен null
+				return false; // возврат значение false
+			}
+		} else if (!nickname.equals(other.nickname)) { // сравнивание никнеймов через equals
+			return false; // возврат значение false
+		}
+		return true; // возврат значение true
+	}
+ 
+	@Override
+	public String toString() {
+		return "Dog { Nickname: " + nickname + " Age: " + age + "}";
+	}
+}
---
+++ src/main/java/object/ObjectPractice.java
@@ -0,0 +1,29 @@
+package object;
+//package ru.itsjava.java_online.object;
+
+import object.Dog;
+
+public class ObjectPractice {
+ 
+	public static void main(String[] args) {
+		Object obj1 = new Object();
+		Object obj2 = new Object();
+ 
+		System.out.println(obj1 == obj2);
+		System.out.println(obj1.equals(obj2)); // сравниваем два разных объекта
+ 
+		obj2 = obj1; // ссылаемся на один и тотже объект
+		System.out.println(obj1.equals(obj2));
+ 
+		Dog gurka = new Dog("Gurka", 10);
+		Dog gurka2 = new Dog("Gurka", 10);
+		Dog murka = new Dog("Murka", 5);
+ 
+		System.out.println("gurka.equals(gurka2): " + gurka.equals(gurka2));
+		System.out.println("gurka.equals(murka): " + gurka.equals(murka));
+ 
+		System.out.println(gurka);
+		System.out.println("test111");
+	}
+ 
+}
