05db3148dac6f9bc3e708ffcf958c41b206d97d9
ShKirill <mr.muld@gmail.com>
2022-03-31 17:42:55 +0300

    add ComparableDz

--- src/main/java/comparable/dz/ComparableDz.java
+++ src/main/java/comparable/dz/ComparableDz.java
@@ -1,9 +1,32 @@
 package comparable.dz;
 
+import java.util.Arrays;
+
 public class ComparableDz {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws CloneNotSupportedException {
+		Person test1 = new Person("test1", "testi", 18);
+		Person test2 = new Person("test2", "testis", 17);
+		Person test3 = new Person("test2", "testis", 57);
+		Person test4 = new Person("test4", "testis", 18);
+		Person test5 = new Person("test5", "testi1", 50);
+
+		Person testClone = (Person) test3.clone();
+
+		System.out.println("CompareTo test1 - test2: " + test1.compareTo(test2));
+		System.out.println("CompareTo test1 - test3: " + test1.compareTo(test3));
+		System.out.println("CompareTo test1 - test4: " + test1.compareTo(test4));
+		System.out.println("CompareTo test1 - test5: " + test1.compareTo(test5));
+		System.out.println("CompareTo test1 - test2: " + test1.compareTo(test2));
+		System.out.println("CompareTo test2 - test3: " + test2.compareTo(test3));
+
+		System.out.println("CompareTo test3 - testClone: " + test3.compareTo(testClone));
+
+		Person[] personArray = { test1, test2, test3, test4, test5, testClone };
+
+		System.out.println(Arrays.toString(personArray));
+		Arrays.sort(personArray);
+		System.out.println(Arrays.toString(personArray));
 	}
 
 }
--- src/main/java/comparable/dz/Person.java
+++ src/main/java/comparable/dz/Person.java
@@ -5,8 +5,34 @@ import lombok.Data;
 
 @AllArgsConstructor
 @Data
-public class Person {
+public class Person implements Cloneable, Comparable<Person> {
 	private final String name;
 	private final String surname;
 	private final int age;
+
+	@Override
+	public int compareTo(Person person) {
+		int result = 0;
+		boolean i = true;
+		if ((surname.equals(person.surname)) == true) {
+			result = 1;
+		} else if ((name.equals(person.name)) == true) {
+			result = 1;
+		} else if (age == person.age) {
+			result = 1;
+		}
+		return result;
+
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name = " + name + ", surname = " + surname + ", age = " + age + "]";
+	}
+	
 }
